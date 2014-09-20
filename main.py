bcfef021c4422e2df22fc468a1881fcbebf33ab1
DrZugrik <zugrik@mail.ru>
2014-09-21 00:27:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-include sys
+import sys
+import PyQt4.Qt
+import math
+from PyQt4 import uic, QtGui
+
+app = QtGui.QApplication(sys.argv)
+win = uic.loadUi("main.ui")
+win.show()
+sys.exit(app.exec_())
